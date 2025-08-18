34b3445e81df6534061c3c0154117b76b2dc1954
dengxinyu <2284799466@qq.com>
2025-08-18 17:08:01 +0800

    11

--- questionnaire-system/client/src/utils/index.js
+++
@@ -1,7 +0,0 @@
-export function formatDate(ts) {
-  const d = new Date(ts)
-  const pad = (n) => String(n).padStart(2, '0')
-  return `${d.getFullYear()}-${pad(d.getMonth()+1)}-${pad(d.getDate())} ${pad(d.getHours())}:${pad(d.getMinutes())}`
-}
-
-
